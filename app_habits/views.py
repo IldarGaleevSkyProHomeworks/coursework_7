5933ae93b74707866cf8f2e0343b5b312ce6d8d5
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-03-15 01:04:24 +0700

    fix permissions for app_habit entities

--- app_habits/views.py
+++ app_habits/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
+from app_accounts.permissions import IsOwner
 from app_habits.models import Habit, HabitNotification
 from app_habits.pagination import AppHabitPagination
 from app_habits.serializers import HabitFullSerializer, HabitPublicSerializer
@@ -12,7 +13,7 @@ from app_habits.serializers.habit_notification_serializer import HabitNotificati
 
 class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitPublicSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
     filterset_fields = ('is_pleasantly', 'periodic')
@@ -21,22 +22,25 @@ class HabitViewSet(viewsets.ModelViewSet):
     pagination_class = AppHabitPagination
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
+        is_swagger = getattr(self, 'swagger_fake_view', False)
+        if not is_swagger and self.request.user.is_superuser:
             return Habit.objects.all()
         return Habit.objects.filter(Q(owner=self.request.user) | Q(is_public=True))
 
     def get_serializer_class(self):
-        if self.request.user.is_superuser:
+        is_swagger = getattr(self, 'swagger_fake_view', False)
+        if not is_swagger and self.request.user.is_superuser:
             return HabitFullSerializer
         return HabitPublicSerializer
 
 
 class HabitNotificationViewSet(viewsets.ModelViewSet):
     serializer_class = HabitNotificationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
+        is_swagger = getattr(self, 'swagger_fake_view', False)
+        if not is_swagger and self.request.user.is_superuser:
             return HabitNotification.objects.all()
         return HabitNotification.objects.filter(owner=self.request.user)
 
